6cd72f5a66b8507d881b0acad24fef90bb22b539
dxps <marius.ileana@gmail.com>
2021-08-04 00:27:55 +0300

    fix(tutor-db): actix&tokio deps, sql; .env file added

--- ezytutors/tutor-db/database.sql
+++ ezytutors/tutor-db/database.sql
@@ -1,21 +1,19 @@
-
 /* Drop table if it already exists*/
 drop table if exists ezy_course_c4;
 /* Create a table. */
 /* Note: Don't put a comma after last field */
 create table ezy_course_c4
 (
-   id serial primary key,
+   course_id serial primary key,
    tutor_id INT not null,
-   name varchar(140) not null,
+   course_name varchar(140) not null,
    posted_time TIMESTAMP default now()
 );
 
 /* Load seed data for testing */
 insert into ezy_course_c4
-   (id,tutor_id, name,posted_time)
+   (course_id,tutor_id, course_name,posted_time)
 values(1, 1, 'First course', '2020-12-17 05:40:00');
 insert into ezy_course_c4
-   (id, tutor_id, name,posted_time)
+   (course_id, tutor_id, course_name,posted_time)
 values(2, 1, 'Second course', '2020-12-18 05:45:00');
-
