cc2de9cf93df767927f35155c549b76a19907d51
dxps <marius.ileana@gmail.com>
2021-08-03 23:25:00 +0300

    update(tutor-db): deps, run_pgdb.sh

--- ezytutors/tutor-db/run_pgdb.sh
+++ ezytutors/tutor-db/run_pgdb.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 docker run -d \
-	--name dev-pgdb \
-	-e POSTGRES_PASSWORD=pass123 \
-	-v ${PROJECT_ROOT}/pgdb_data:/var/lib/postgresql/data \
+	--name eztutors-pgdb \
+	-e POSTGRES_USER=ezt \
+	-e POSTGRES_PASSWORD=ezpass \
+	-e POSTGRES_DB=ezdb \
+	-v ${PROJECT_ROOT}/pgdb:/var/lib/postgresql/data \
     -p 5432:5432 \
-    postgres
+    postgres:13
 
