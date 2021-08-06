e8eb89e75e6bc03515aef6851b31e9a278f09729
dxps <marius.ileana@gmail.com>
2021-08-06 22:51:15 +0300

    lint(tutor-db): handlers unused args

--- ezytutors/tutor-db/src/iter2/handlers.rs
+++ ezytutors/tutor-db/src/iter2/handlers.rs
@@ -11,22 +11,22 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
 }
 
 pub async fn get_courses_for_tutor(
-    app_state: web::Data<AppState>,
-    params: web::Path<(usize,)>,
+    _app_state: web::Data<AppState>,
+    _params: web::Path<(usize,)>,
 ) -> HttpResponse {
     HttpResponse::Ok().json("ok")
 }
 
 pub async fn get_course_details(
-    app_state: web::Data<AppState>,
-    params: web::Path<(usize, usize)>,
+    _app_state: web::Data<AppState>,
+    _params: web::Path<(usize, usize)>,
 ) -> HttpResponse {
     HttpResponse::Ok().json("ok")
 }
 
 pub async fn post_new_course(
-    app_state: web::Data<AppState>,
-    new_course: web::Json<Course>,
+    _app_state: web::Data<AppState>,
+    _new_course: web::Json<Course>,
 ) -> HttpResponse {
     HttpResponse::Ok().json("ok")
 }
