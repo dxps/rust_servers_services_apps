a03f2b84ae9f911f2b6d6a7c867ce24594e4d1d8
dxps <marius.ileana@gmail.com>
2021-09-05 19:04:40 +0300

    feat(ezytutors_v2/tutor-web-app-ssr): iter5 show registration form

--- ezytutors_v2/tutor-web-app-ssr/readme.md
+++ ezytutors_v2/tutor-web-app-ssr/readme.md
@@ -18,7 +18,12 @@ These iterations show how to rendered different elements in HTML using Tera temp
   - Run it and access http://localhost:8080/tutors
 
 - iter 4
+
   - It shows how to fetch the tutors from an external source and then render the dynamic list in the page.
   - First, go to `tutor-web-service` and run `./run_iter5.sh`
   - Next, in this `tutor-web-app-ssr` project, run `./run_iter4.sh`
   - Go to http://localhost:8080/tutors to see the result.
+
+- iter 5
+  - It shows the registration form as the langing page (accessing the '/' path).
+  - Registration (of both user and tutor data) submit creates the user in this tutor-web-app-ssr (in its database) and the tutor in the tutor-web-service, called by this back-end handler.
--- ezytutors_v2/tutor-web-app-ssr/src/iter5/handlers.rs
+++ ezytutors_v2/tutor-web-app-ssr/src/iter5/handlers.rs
@@ -1,8 +1,23 @@
-use actix_web::{Error, HttpResponse, Result};
+use actix_web::{web, Error, HttpResponse, Result};
 
-pub async fn show_register_form() -> Result<HttpResponse, Error> {
-    let msg = "Hello, you are in the registration page";
-    Ok(HttpResponse::Ok().content_type("text/html").body(msg))
+use crate::iter5::errors::EzyTutorError;
+
+pub async fn show_register_form(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("error", "");
+    ctx.insert("current_username", "");
+    ctx.insert("current_password", "");
+    ctx.insert("current_confirmation", "");
+    ctx.insert("current_name", "");
+    ctx.insert("current_imageurl", "");
+    ctx.insert("current_profile", "");
+
+    let s = tmpl.render("register.html", &ctx).map_err(|err| {
+        println!("show_register_form> Render err: {}", err);
+        EzyTutorError::TeraError("Template error".into())
+    })?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 pub async fn handle_register() -> Result<HttpResponse, Error> {
