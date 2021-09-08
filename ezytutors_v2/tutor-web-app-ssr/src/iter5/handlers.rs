eabcc34f06d3b5afb9236976099398d258fcbf9b
dxps <marius.ileana@gmail.com>
2021-09-08 20:19:23 +0300

    fix(ezytutors_v2/tutor-web-app-ssr): iter5 solved the tutor creation during registration

--- ezytutors_v2/tutor-web-app-ssr/src/iter5/handlers.rs
+++ ezytutors_v2/tutor-web-app-ssr/src/iter5/handlers.rs
@@ -1,4 +1,7 @@
-use actix_web::{web, Error, HttpResponse, Result};
+use actix_web::{
+    web::{self, Bytes},
+    Error, HttpResponse, Result,
+};
 use argon2::Config;
 use serde_json::json;
 
@@ -33,6 +36,7 @@ pub async fn handle_register(
     app_state: web::Data<AppState>,
     params: web::Form<TutorRegisterForm>,
 ) -> Result<HttpResponse, Error> {
+    println!("handle_register> begin");
     let mut ctx = tera::Context::new();
     let s;
     let username = params.username.clone();
@@ -58,14 +62,37 @@ pub async fn handle_register(
                 "tutor_profile": &params.profile
             });
             let awc_client = awc::Client::default();
-            let res = awc_client
-                .post("http://localhost:3000/tutors/")
+            let mut res = awc_client
+                .post("http://localhost:3000/tutors")
+                .content_type("application/json")
                 .send_json(&new_tutor)
                 .await
-                .unwrap()
-                .body()
-                .await?;
-            let tutor_response: TutorResponse = serde_json::from_str(&std::str::from_utf8(&res)?)?;
+                .unwrap();
+            let res_status = &res.status();
+            let res_body_bytes = &res.body().await.unwrap_or_else(|err| {
+                println!("Error calling WS: '{}' (status={})", err, res_status);
+                Bytes::new()
+            });
+            let ws_res = std::str::from_utf8(&res_body_bytes)?;
+            println!(
+                "Got WS response body: '{}' (length={}, status={})",
+                ws_res,
+                ws_res.len(),
+                res_status
+            );
+            let tutor_response: TutorResponse =
+                serde_json::from_str(&ws_res).unwrap_or_else(|err| {
+                    println!(
+                        "Error parsing ws response: '{}' having response: '{}' ",
+                        err, &ws_res
+                    );
+                    TutorResponse {
+                        tutor_id: 0,
+                        tutor_name: "ERR".into(),
+                        tutor_pic_url: "".into(),
+                        tutor_profile: "".into(),
+                    }
+                });
             s = format!("Congratulations. You have been successfully registered with EzyTutor and your tutor id is: {}. To start using EzyTutor, please login with your credentials.",tutor_response.tutor_id);
             // Hash the password
             let salt = b"somerandomsalt";
